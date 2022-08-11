874215032f8e7399f0a0bf345dcc95c9bb8db461
Grey Elerson <grey.elerson@fauna.com>
2022-08-10 19:17:57 -0600

    handle state and pageload event

--- mays_react/src/components/Preloader.js
+++ mays_react/src/components/Preloader.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Preloader = () => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    if (document.readyState === 'complete') {
+      setLoaded(true);
+    } else {
+      window.addEventListener('load', () => {setLoaded(true)});
+      // cleanup event listener on unmount
+      return () => window.removeEventListener('load', () => {setLoaded(true)});
+    }
+  }, [document.readyState])
+
   return (
-    <div id="preloader">
-        <div className="preloader">
-            <span></span>
-            <span></span>
-        </div>
-    </div>
+    <>
+    {loaded == false && 
+      <div id="preloader">
+          <div className="preloader">
+              <span></span>
+              <span></span>
+          </div>
+      </div>
+    }
+    </>
   );
 };
 
