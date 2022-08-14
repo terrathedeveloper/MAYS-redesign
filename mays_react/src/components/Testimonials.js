8df25971d0bda2ade66a753cdde272b3dabbafef
Grey Elerson <grey.elerson@fauna.com>
2022-08-13 19:17:02 -0600

    Change testimonials to react-responsive-carousel

--- mays_react/src/components/Testimonials.js
+++ mays_react/src/components/Testimonials.js
@@ -1,23 +1,41 @@
 import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 
 const Testimonials = () => {
+
+    const backgroundImgCarouselSettings = {
+        showThumbs: false,
+        showArrows: false,
+        showStatus: false,
+        showIndicators: false,
+    }
+
+    const testimonialContentCarouselSettings = {
+        showThumbs: false,
+        showArrows: false,
+        showStatus: false,
+        showIndicators: true,
+        autoPlay: true,
+        infiniteLoop: true,
+        interval: 3500,
+    }
+
   return (
     <section className="testimonial_area">
         <div className="testimonial_image_wrapper_2">
-            <div className="single_testimonial_image_2">
-                <img src="images/tents.jpg" alt="testimonial" />
-            </div> 
-            {/* <!-- single testimonial image --> */}
-            
-            <div className="single_testimonial_image_2">
-                <img src="images/testimonial-2.jpg" alt="testimonial" />
-            </div> 
-            {/* <!-- single testimonial image --> */}
-            
-            <div className="single_testimonial_image_2">
-                <img src="images/testimonial-3.jpg" alt="testimonial" />
-            </div> 
-            {/* <!-- single testimonial image --> */}
+            <Carousel {...backgroundImgCarouselSettings}>
+                <div className="single_testimonial_image_2">
+                    <img src="images/tents.jpg" alt="testimonial" />
+                </div> 
+                {/* <!-- single testimonial image --> */}
+                
+                {/* <div className="single_testimonial_image_2">
+                    <img src="images/testimonial-2.jpg" alt="testimonial" />
+                </div>  */}
+                {/* <!-- single testimonial image (for reference) --> */}
+            </Carousel>
         </div> 
         {/* <!-- testimonial image wrapper --> */}
         
@@ -26,31 +44,32 @@ const Testimonials = () => {
                 <div className="row justify-content-end">
                     <div className="col-lg-6">
                         <div className="testimonial_content_wrapper_2">
-                           
-                            <div className="single_testimonial_content text-center">
-                                <i className="fa fa-quote-right"></i>
-                                <h4 className="testimonial_title">Testimonial</h4>
-                                <p>I noticed a difference in my son the very next day after our family assessment. He remembers what brings him joy, and he's committed to reconnecting to it. </p>
-                                <h6 className="author_name">Tamira Smith</h6>
-                            </div> 
-                            {/* <!-- single testimonial content --> */}
-                           
-                            <div className="single_testimonial_content text-center">
-                                <i className="fa fa-quote-right"></i>
-                                <h4 className="testimonial_title">Testimonial</h4>
-                                <p>Thank you SO MUCH for bringing us our child back! You've been the blessing I prayed so long for!</p>
-                                <h6 className="author_name">Imani Nelson</h6>
-                            </div> 
-                            {/* <!-- single testimonial content --> */}
-                           
-                            <div className="single_testimonial_content text-center">
-                                <i className="fa fa-quote-right"></i>
-                                <h4 className="testimonial_title">Testimonial</h4>
-                                <p>It's been two years since I sent Brandon on the camping trip, and I still see him using the tools taught to him in his daily life. </p>
-                                <h6 className="author_name">Ronnell Cook</h6>
-                            </div> 
-                            {/* <!-- single testimonial content --> */}
+                            <Carousel {...testimonialContentCarouselSettings} animationHandler="fade">
                             
+                                <div className="single_testimonial_content text-center">
+                                    <i className="fa fa-quote-right"></i>
+                                    <h4 className="testimonial_title">Testimonial</h4>
+                                    <p>I noticed a difference in my son the very next day after our family assessment. He remembers what brings him joy, and he's committed to reconnecting to it. </p>
+                                    <h6 className="author_name">Tamira Smith</h6>
+                                </div> 
+                                {/* <!-- single testimonial content --> */}
+                            
+                                <div className="single_testimonial_content text-center">
+                                    <i className="fa fa-quote-right"></i>
+                                    <h4 className="testimonial_title">Testimonial</h4>
+                                    <p>Thank you SO MUCH for bringing us our child back! You've been the blessing I prayed so long for!</p>
+                                    <h6 className="author_name">Imani Nelson</h6>
+                                </div> 
+                                {/* <!-- single testimonial content --> */}
+                            
+                                <div className="single_testimonial_content text-center">
+                                    <i className="fa fa-quote-right"></i>
+                                    <h4 className="testimonial_title">Testimonial</h4>
+                                    <p>It's been two years since I sent Brandon on the camping trip, and I still see him using the tools taught to him in his daily life. </p>
+                                    <h6 className="author_name">Ronnell Cook</h6>
+                                </div> 
+                                {/* <!-- single testimonial content --> */}
+                            </Carousel>
                         </div> 
                         {/* <!-- row --> */}
                     </div>
